bfdb6bbbc3d8fd58ff55b2c072af05a841b45c71
haruyq <156767674+haruyq@users.noreply.github.com>
2026-04-18 21:51:01 +0900

    deps_dirもスキャンするように

--- src/router/mods/list.rs
+++ src/router/mods/list.rs
@@ -2,6 +2,7 @@ use axum::{Json, Router, routing::get};
 use serde_json as json;
 use std::path::PathBuf;
 use sha2::{Sha256, Digest};
+use tokio::join;
 use tokio::fs;
 use tokio::fs::File;
 use tokio::io::AsyncReadExt;
@@ -26,30 +27,46 @@ async fn get_sha256(path: &str) -> std::io::Result<String> {
     Ok(hex::encode(hasher.finalize()))
 }
 
-async fn list_mods() -> Json<json::Value> {
-    let config = config::load_config();
-    let mods_dir = PathBuf::from(&config.mods_dir);
-    let mut mods = Vec::new();
+async fn scan_dir(dir: PathBuf) -> Vec<json::Value> {
+    let mut result = Vec::new();
 
-    if let Ok(mut entries) = fs::read_dir(mods_dir).await {
-
+    if let Ok(mut entries) = fs::read_dir(dir).await {
         while let Ok(Some(entry)) = entries.next_entry().await {
             let path = entry.path();
-            if path.is_file() &&
-                path.extension().and_then(|ext| ext.to_str()) == Some("jar") {
-                    let sha256 = get_sha256(path.to_str().unwrap_or(""))
-                        .await
-                        .unwrap_or_default();
 
-                    if let Some(file_name) = path.file_name().and_then(|n| n.to_str()) {
-                        mods.push(json::json!({
-                            "name": file_name,
-                            "sha256": sha256,
-                        }));
+            if path.is_file()
+                && path.extension().and_then(|e| e.to_str()) == Some("jar")
+            {
+                let sha256 = get_sha256(path.to_str().unwrap_or(""))
+                    .await
+                    .unwrap_or_default();
+
+                if let Some(file_name) = path.file_name().and_then(|n| n.to_str()) {
+                    result.push(json::json!({
+                        "name": file_name,
+                        "sha256": sha256,
+                    }));
                 }
             }
         }
     }
 
-    Json(json::json!(mods))
+    result
+}
+
+async fn list_mods() -> Json<json::Value> {
+    let config = config::load_config();
+
+    let mods_dir = PathBuf::from(&config.mods_dir);
+    let deps_dir = PathBuf::from(&config.deps_dir);
+
+    let (mods, deps) = join!(
+        scan_dir(mods_dir),
+        scan_dir(deps_dir)
+    );
+
+    let mut all = mods;
+    all.extend(deps);
+
+    Json(json::json!(all))
 }
